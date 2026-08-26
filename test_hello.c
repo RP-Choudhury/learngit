f94c477ad46fbc09310699dfdf1c9a7d2e00025e
Rudra <workrudra200718@gmail.com>
2026-08-27 00:25:59 +0530

    Add functionality to be able to square integers.

--- test_hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    printf("hello.c is tested.")
-}
-
-// this is a comment
---
+++ test_hello_and_square.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int sqaure(int n);
+int main(void)
+{
+    printf("hello.c is tested.")
+    printf("sqaure functionality can be tested.")
+}
+
+int square(int n)
+{
+    return n*n;
+}
+// this is a comment
